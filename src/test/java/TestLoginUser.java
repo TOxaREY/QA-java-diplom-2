e9921e4ceccb4952197154f651f188456e7ecd33
Anton Reynikov <toxareyapp@yandex.ru>
2022-10-04 17:58:10 +0300

    add login user tests

--- src/test/java/TestLoginUser.java
+++ src/test/java/TestLoginUser.java
@@ -17,84 +17,77 @@ import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.Matchers.notNullValue;
 import static org.hamcrest.core.Is.is;
 
-@DisplayName("Тесты создания пользователя")
-public class TestCreateUser {
+@DisplayName("Тесты авторизации пользователя")
+public class TestLoginUser {
     private String accessToken;
-    private Response response(File json) {
+    private Response response(File json, String path) {
         return given().header("Content-type", "application/json")
                 .and()
                 .body(json)
                 .when()
-                .post("/api/auth/register");
+                .post("/api/auth/" + path);
     }
     private void setAccessToken(Response response) {
         JsonPath jsonPathEvaluator = response.jsonPath();
         accessToken = jsonPathEvaluator.get("accessToken").toString();
     }
-    private void testNegative(Response response, String message) {
+
+    private void testNegative(Response response) {
         response.then().assertThat()
-                .statusCode(403)
+                .statusCode(401)
                 .and()
                 .body("success", is(false))
                 .and()
-                .body("message", equalTo(message));
+                .body("message", equalTo("email or password are incorrect"));
     }
+
     Gson g = new Gson();
+
     @Before
     public void setUp() {
         RestAssured.baseURI = Uri.BASEURI;
     }
 
     @Test
-    @DisplayName("Тест создания уникального пользователя")
-    public void testUniqueUserCreation() throws FileNotFoundException {
+    @DisplayName("Тест авторизации под существующим пользователем")
+    public void testLoginExistingUser() throws FileNotFoundException {
         File json = new File("src/test/resources/userFull.json");
         BufferedReader br = new BufferedReader(new FileReader("src/test/resources/userFull.json"));
         User user = g.fromJson(br, User.class);
-        Response response = response(json);
-        response.then().assertThat()
+        Response response = response(json, "register");
+        setAccessToken(response);
+        response(json, "login").then().assertThat()
                 .statusCode(200)
                 .and()
                 .body("success", is(true))
                 .and()
-                .body("user.email", equalTo(user.email))
-                .and()
-                .body("user.name", equalTo(user.name))
-                .and()
                 .body("accessToken", notNullValue())
                 .and()
-                .body("refreshToken", notNullValue());
-        setAccessToken(response);
+                .body("refreshToken", notNullValue())
+                .and()
+                .body("user.email", equalTo(user.email))
+                .and()
+                .body("user.name", equalTo(user.name));
     }
 
     @Test
-    @DisplayName("Тест создания пользователя, который уже зарегистрирован")
-    public void testCreateUserWhoIsAlreadyRegistered() {
+    @DisplayName("Тест авторизации с неверным логином")
+    public void testLoginWrongLogin() {
         File json = new File("src/test/resources/userFull.json");
-        Response response = response(json);
+        File jsonWrongLogin = new File("src/test/resources/userWrongName.json");
+        Response response = response(json, "register");
         setAccessToken(response);
-        testNegative(response(json), "User already exists");
+        testNegative(response(jsonWrongLogin, "login"));
     }
 
     @Test
-    @DisplayName("Тест создания пользователя без поля email")
-    public void testCreateUserWithoutEmailField() {
-        File json = new File("src/test/resources/userWithoutEmailField.json");
-        testNegative(response(json), "Email, password and name are required fields");
-    }
-
-    @Test
-    @DisplayName("Тест создания пользователя без поля password")
-    public void testCreateUserWithoutPasswordField() {
-        File json = new File("src/test/resources/userWithoutPasswordField.json");
-        testNegative(response(json), "Email, password and name are required fields");
-    }
-
-    @Test
-    @DisplayName("Тест создания пользователя без поля name")
-    public void testCreateUserWithoutNameField() {
-        File json = new File("src/test/resources/userWithoutNameField.json");
-        testNegative(response(json), "Email, password and name are required fields");
+    @DisplayName("Тест авторизации с неверным паролем")
+    public void testLoginWrongPassword()  {
+        File json = new File("src/test/resources/userFull.json");
+        File jsonWrongPassword = new File("src/test/resources/userWrongPassword.json");
+        Response response = response(json, "register");
+        setAccessToken(response);
+        testNegative(response(jsonWrongPassword, "login"));
     }
 
     @After
